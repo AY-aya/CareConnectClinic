60d704ee26e4df44c99dbfc68aeced3d91f00ac3
AY-aya <160540583+AY-aya@users.noreply.github.com>
2024-05-05 23:18:23 +0300

    Update Patient.java

--- src/Patient.java
+++ src/Patient.java
@@ -1,34 +1,27 @@
 package com.mycompany.careconnectclinic;
 
-//import jdk.internal.icu.impl.CharacterIteratorWrapper;
-
-import java.sql.*;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class Patient {
-    private String name;
-    private String patientID;
-    private String phonenumber;
-    private int age;
-    private String gender;
-    private String allergies;
-    private String health_problems;
-    private String medications;
-    private String diagnosis;
-    private static int patientIDcounter=0;
-    static ResultSet resultSet = null;
-
-    public Patient(String name,String phonenumber ,int age,String gender){
-        this.name=name;
-        this.phonenumber=phonenumber;
+     private String name;
+     private String patientID;
+     private String phonenumber;
+     private int age;
+     private String gender;
+     private String allergies;
+     private String health_problems;
+     private String medications;
+     private String diagnosis;
+     private static int patientIDcounter=0;
+    
+     public Patient(String name,String phonenumber ,int age,String gender){
+     this.name=name;
+     this.phonenumber=phonenumber;
         if (phonenumber.length()!=11)
             System.out.println("invalid phone number");
-        this.age=age;
-        this.gender=gender;
-        patientIDcounter++;
-        patientID= "PT"+ patientIDcounter;
-    }
+     this.age=age;
+     this.gender=gender;
+     patientIDcounter++;
+     patientID= "PT"+ patientIDcounter;
+   } 
 
     public String getAllergies() {
         return allergies;
@@ -61,7 +54,7 @@ public class Patient {
     public void setDiagnosis(String diagnosis) {
         this.diagnosis = diagnosis;
     }
-
+    
     public String getName() {
         return name;
     }
@@ -69,7 +62,7 @@ public class Patient {
     public void setName(String name) {
         this.name = name;
     }
-
+     
     public String getPatientID() {
         return patientID;
     }
@@ -97,38 +90,5 @@ public class Patient {
     public void setGender(String gender) {
         this.gender = gender;
     }
-
-    public static void getReport(int id) throws SQLException {
-
-        mySqlCon database = new mySqlCon();
-        database.connect();
-        resultSet = database.getQuery("SELECT * FROM patient");
-
-        // 4. Process the results
-
-        while (resultSet.next()) {
-
-            int patientId = resultSet.getInt("PId");
-
-            if (patientId == id) {
-                String name = resultSet.getString("PName");
-                int age = resultSet.getInt("PAge");
-                String gender = resultSet.getString("PGender");
-                String allergies = resultSet.getString("PAllergies");
-                String health_problems = resultSet.getString("PHealth_problems");
-                String medications = resultSet.getString("PMedications");
-
-                // Retrieve other columns as needed
-
-                System.out.println("Patient ID: " + patientId + ", Name: " + name + " Gender: " + gender + " allergies: " + allergies);
-                // Print other retrieved data
-            }
-        }
-        database.disconnect();
-
-    }
-    public static void main(String[] args) throws SQLException {
-        getReport(2);
-    }
-
+     
 }
